4e3c9e1fce122d4bf6b534edcb0c931d2031d540
Andrey <kupihleba@yandex.ru>
2018-03-11 18:41:11 +0300

    checkpoint cube

--- openGL_lab2/res/shaders/basic.shader
+++ openGL_lab2/res/shaders/basic.shader
@@ -2,11 +2,22 @@
 #version 330 core
 layout(location = 0) in vec4 position;
 uniform mat4 myTransformation;
+uniform float y_angle;
+uniform float timer;
 
+mat4 rotate_y(float a)
+{
+	return mat4(
+		vec4(cos(a),  0.0, sin(a), 0.0),
+		vec4(  0.0,   1.0,  0.0,   0.0),
+		vec4(-sin(a), 0.0, cos(a), 0.0),
+		vec4(  0.0,   0.0,  0.0,   1.0)
+	);
+}
 
 void main()
 {
-	gl_Position = position * myTransformation;
+	gl_Position = position * myTransformation * rotate_y(y_angle);
 }
 
 #fragment
--- openGL_lab2/src/Cube.cpp
+++ openGL_lab2/src/Cube.cpp
@@ -2,7 +2,9 @@
 
 #define DIM 3	// dimension
 #define PT_PER_EDGE 4
+#define COLOURS 6 * 3
 #define EDGES 6 // number of edges 
+#define PI 3.141592654
 
 using namespace obj;
 
@@ -10,54 +12,110 @@ Cube::Cube(GLuint shader)
 {
 	_shader = shader;
 
-	float data[DIM * EDGES * PT_PER_EDGE] = {
+	float data[DIM * EDGES * PT_PER_EDGE /*+ COLOURS*/] = {
+		//0.0f,  0.0f, 1.0f,
+		//    BOTTOM    //
+		-1.0f,  1.0f, -1.0f,
+		 1.0f,  1.0f, -1.0f,
+		 1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,
+		
+		
+		//0.0f,  0.0f, 5.0f,
 		 //     TOP      //
 		 1.0f,  1.0f, 1.0f,
 		 1.0f, -1.0f, 1.0f,
 		-1.0f, -1.0f, 1.0f,
 		-1.0f,  1.0f, 1.0f,
 
-		 //    BOTTOM    //
-		 1.0f,  1.0f, -1.0f,
-		 1.0f, -1.0f, -1.0f,
-		-1.0f, -1.0f, -1.0f,
-		-1.0f,  1.0f, -1.0f,
-
+		 //0.0f,  0.5f, 0.0f,
 		 //     LEFT     //
-		 1.0f, -1.0f, 1.0f,
-		-1.0f, -1.0f, 1.0f,
 		 1.0f, -1.0f, -1.0f,
+		 1.0f, -1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,
 		-1.0f, -1.0f, -1.0f,
 
-		 //    RIGHT     //
-		 1.0f,  1.0f, 1.0f,
-		-1.0f,  1.0f, 1.0f,
-		 1.0f,  1.0f, -1.0f,
-		-1.0f,  1.0f, -1.0f,
+		
 
-		 //     FACE     //
-		 1.0f, -1.0f, -1.0f,
+		 //0.0f,  0.1f, 0.0f,
+		 //    RIGHT     //
+		-1.0f,  1.0f, -1.0f,
 		 1.0f,  1.0f, -1.0f,
 		 1.0f,  1.0f,  1.0f,
-		 1.0f, -1.0f,  1.0f,
-
-		 //     BACK     //
-		-1.0f, -1.0f, -1.0f,
-		-1.0f,  1.0f, -1.0f,
 		-1.0f,  1.0f,  1.0f,
-		-1.0f, -1.0f,  1.0f
+
+		//0.5f,  0.0f, 0.0f,
+		//     FACE     //
+		1.0f, -1.0f, -1.0f,
+		1.0f,  1.0f, -1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f, -1.0f,  1.0f
+
+		//1.0f,  0.0f, 0.0f,
+		//     BACK     //
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f,  1.0f,
+		-1.0f,  1.0f,  1.0f,
+		-1.0f,  1.0f, -1.0f,
+
+		 
 	};
+	/*float data[] = {
+		// Позиции             // Цвета
+		-0.5f, -0.5f, -0.5f,   0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f,    1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,    1.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,    1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,   0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,   0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,   0.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,    1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,    1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,    1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,   0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,   0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,   1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,   1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,   0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,   0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,   0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,   1.0f, 0.0f,
+
+		0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,    1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,    0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,    0.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,    0.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,   0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,    1.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,    1.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,    1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,   0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,   0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,   0.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,    1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,   0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,   0.0f, 1.0f
+	};*/
 	for (int i = 0; i < DIM * EDGES * PT_PER_EDGE; i++) {
-		data[i] /= 2.f;
+		data[i] /= 2.0f;
 	}
 
 
 	glGenBuffers(1, &_buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, _buffer);
-	glBufferData(GL_ARRAY_BUFFER, DIM * EDGES * PT_PER_EDGE * sizeof(float), data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (GLvoid*)0);
 	glBindVertexArray(0);
 }
 
@@ -70,22 +128,27 @@ Cube::~Cube()
 
 void obj::Cube::draw()
 {
-	float k = 2.0f;
+	GLfloat timeValue = glfwGetTime();
+	float k = 1.0f;
 	const int SZ = 16;
+	float fi = timeValue;
 	float norm[SZ] = {
-		1.0f, 0.0f, 0.0f, 0.0f,
-		0.0f,  k,   0.0f, 0.0f,
-		0.0f, 0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 0.0f, 1.0f,
+		1.0f, 0.0f,      0.0f,    0.0f,
+		0.0f, k*cos(fi), sin(fi), 0.0f,
+		0.0f, -sin(fi),  cos(fi), 0.0f,
+		0.0f, 0.0f,      0.0f,    1.0f,
 	};
 
-	GLfloat timeValue = glfwGetTime();
+	
 	GLfloat greenValue = (sin(timeValue) / 2) + 0.5;
 	GLint vertexColorLocation = glGetUniformLocation(_shader, "myColor");
 	GLint myTranformation = glGetUniformLocation(_shader, "myTransformation");
+	GLint y_angle = glGetUniformLocation(_shader, "y_angle");
 
+	glUniform1f(y_angle, fi);
 	glUseProgram(_shader);
 	glUniform4f(vertexColorLocation, 0.0f, greenValue, 1.0f, 1.0f);
+	//glMatrixMode(GL_PROJECTION);
 	glUniformMatrix4fv(myTranformation, 1, GL_FALSE, norm);
 
 	//glBindBuffer(GL_ARRAY_BUFFER, _buffer);
@@ -101,8 +164,6 @@ void obj::Cube::draw()
 	//glRotatef(45.0f, 0.0f, 1.0f, 0.0f);
 	//glRotatef(60.0f, 1.0f, 0.0f, 0.0f);
 	glBindVertexArray(_buffer);
-	glDrawArrays(GL_QUADS, 0, 4);
+	glDrawArrays(GL_QUADS, 0, 24*2);
 	glBindVertexArray(0);
 }
-
-
